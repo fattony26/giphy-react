8e37b95b751bf31ad2b4e90ac8fd535bc2414895
fattony26 <marco.moran26@hotmail.com>
2021-11-12 11:26:23 -0500

    deleting unnecessary files

--- src/App.js
+++ src/App.js
@@ -14,18 +14,48 @@ class App extends Component {
     }
   }
 
+  handleChange = (event) => {
+    this.setState({ 
+      [event.target.id]: event.target.value
+    })
+  }
 
-  
+  handleSubmit = (event) => {
+    event.preventDefault()
+    this.setState({
+      searchURL: this.state.baseURL + this.state.apikey + this.state.query + this.state.search
+    }, () => {
+      fetch(this.state.searchURL)
+        .then(res => res.json())
+        .then(json => this.setState({
+          giphy: json,
+          search: ''
+        }))
+        .catch(err => console.error(err));
+    })
+  }
+
   render() {
     return (
       <>
+      <form onSubmit={this.handleSubmit}>
+          <label htmlFor='search'>Giphy</label>
+          <input
+            type='text'
+            value={this.state.search}
+            onChange={this.handleChange}
+          />
+          <input
+            type='submit'
+            value='Search'
+          />
+        </form>
         <h1>Giphy</h1>
         <input type=" text" placeholder="Giphy Search" name="Gif Search"></input>
           <button type="submit">Search</button>
       </>
     )
   }
-
 }
 
 export default App;
